2729920c96314d44b37181133773f14a6c60dd7e
CardosoTephy <126803328+CardosoTephy@users.noreply.github.com>
2024-06-19 13:26:06 -0300

    alteraçoes dos codigos e add codigos ao style

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const caixaPrincipal = document.querySelector(".caixa-principal");
+onst caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
@@ -9,24 +9,23 @@ const perguntas = [
         enunciado: "Quais os métodos utilizados na sua cidade para combater o crime?",
         alternativas: [
             "Câmera de segurança",
-            "Sensor de segurança",
-        ]
-    },
-    {   
-        enunciado: " quais são os fatores sociais que contribuem para a ocorrência de crimes,como roubo,violência e corrupção?",
-        alternativas: [
-            "Fome",
-            "desenprego"
+            "Sensor e de segurança"
         ]
     },
     {
-        enunciado: "Quias são os crimes mais ocorrents em sua cidade?",
+        enunciado: "Quais são os fatores sociais que contribuem para o ocorrência de crimes, como roubo, violência e corrupção?  ",
+        alternativas: [
+            "Fome",
+            "Desemprego"
+        ]
+    },
+    {
+        enunciado: "Quais ão os crimes mai ocorrentes na sua cidade?",
         alternativas: [
             "Tráfico de drogas",
             "Roubo"
         ]
     },
-   
 ];
 
 let atual = 0;
@@ -35,6 +34,14 @@ let perguntaAtual;
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
 }
 
 mostraPergunta();

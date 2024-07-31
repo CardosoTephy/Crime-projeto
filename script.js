2cbe47438a04a25d6ccef6c298a91f8afc03e993
CardosoTephy <126803328+CardosoTephy@users.noreply.github.com>
2024-07-31 13:57:18 -0300

    alterei tudo

--- script.js
+++ script.js
@@ -10,42 +10,42 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Câmera de segurança",
-                afirmacao: "Onde foi instalado câmeras pela cidade inteira e caso precise é tudo filmado."
+                afirmacao: "onde foi instalado câmeras pela cidade inteira, e caso precise é tudo filmado"
             },
             {
-                texto: "Sensor e de segurança"
-                afirmacao: "Quando alguém tenta invadir algum comércio ou algum lugar que temha este sensor ele é disparado e é direcionado o sinal para a delegacia."
+                texto: "Sensor e de segurança",
+                afirmacao: "quando alguém tenta invadir algum comércio ou algum lugar que tenha este sensor ele é disparado e é direcionado o sinal para delegacia "
             }
         ]
     },
     {
-        enunciado: "Quais são os fatores sociais que contribuem para o ocorrência de crimes, como roubo, violência e corrupção?  ",
+        enunciado: "Quais são os fatores sociais que contribuem para o ocorrência de crimes, como roubo, violência e corrupção? ",
         alternativas: [
             {
                 texto: "Fome",
-                afirmacao: "É um dos principais fatores da ocorrência do roubo a fome leva o indivíduo a roubar para se alimentar."
+                afirmacao: "é um dos principais fatores da ocorrência do roubo, a fome leva o indivíduo a roubar para se alimentar"
             },
             {
-                texto: "Desemprego"
-                afirmacao: "E outro dos principais fatores para o crime,eles roubam para conseguir dinheiro e se sustentarem."
+                texto: "Desemprego",
+                afirmacao: "é outro dos principais fatores para o crime, eles roubam para conseguir dinheiro e se sustentarem"
             }
         ]
-        
     },
     {
         enunciado: "Quais ão os crimes mais ocorrentes na sua cidade?",
         alternativas: [
             {
                 texto: "Tráfico de drogas",
-                afirmacao: "É a atividade criminosa que envolve a produção,distribuição e venda ilegal de substâncias entorpecentes"
+                afirmacao: "é a atividdade criminosa que envolve a produção e distribuição e venda ilegal de substâncias entorpecentes"
             },
             {
-                texto: "Roubo"
-                afirmacao: "È um crime contra o patrimônio que envolve a apropriação indebita de bens ou recursos de outra pessoa por meio de ameaças,violência ou intimidação,pode ocorrer em diversos contextos,como assaltos a mão armada,furtos em residências,roubos a estabelecimentos comerciais,etc. "
-            }enunciado
+                texto: "Roubo",
+                afirmacao: "é um crime contra o patrimônio que envolve a apropriação indébita de bens ou recursos de outra pessoa por meio de ameaça, violência, ou intimidação, pode ocorrer em diversos contextos, como assaltos a mão armada, furtos em residências, roubos a estabelcimentos comerciais, etc."
+            }
         ]
     },
 ];
+
 
 let atual = 0;
 let perguntaAtual;
@@ -79,7 +79,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "Nesta atualidade...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
